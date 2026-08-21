5754a6fa883ed6f5e31ca7b151ab50bd1b001bdd
ysjeondev <hpc9909@gmail.com>
2026-08-21 20:39:51 +0900

    교재 doit 디장고 116까지 완료_260821

--- projects/projectmysite/pybo/forms.py
+++ projects/projectmysite/pybo/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 from pybo.models import Question
+
 class QuestionForm(forms.ModelForm):
     class Meta:
-        model = Questionfields = ['subject','content']
+        model = Question
+        fields = ['subject','content']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class':'form-control','rows':10}),
+        }
