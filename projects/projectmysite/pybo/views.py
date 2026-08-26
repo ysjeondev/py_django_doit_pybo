ae68df39a61d6de9d83e63b7b4ec6efcc597270d
ysjeondev <hpc9909@gmail.com>
2026-08-26 19:36:48 +0900

    교재 doit 디장고 121까지 완료_260826

--- projects/projectmysite/pybo/views.py
+++ projects/projectmysite/pybo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 def index(request):
@@ -20,9 +20,21 @@ def detail(request,question_id):
 def answer_create(request, question_id):
     #pybo 답변 등록
     question = get_object_or_404(Question, pk=question_id)
+    if request.method =="POST":
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail',question_id=question.id)
+    else:
+        form =AnswerForm()
+        context ={'question':question, 'form':form}
+        return render(request,'pybo/question_detail.html',context)
     question.answer_set.create(content=request.POST.get('content'),
         create_date=timezone.now())
     return redirect('pybo:detail',question_id=question.id)
+
 
 def question_create(request):
     #pybo 질문 등록
